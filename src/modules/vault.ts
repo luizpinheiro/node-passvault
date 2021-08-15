729937e9f872c16e47a413280c2fd8a767c20f36
Luiz Claudio Pinheiro Alves <luiz.alves@bancointer.com.br>
2021-08-15 13:17:13 -0300

    Adding relevante comments to the vault module

--- src/modules/vault.ts
+++ src/modules/vault.ts
@@ -10,8 +10,15 @@ let vault: Vault | null = null
 let vaultPassword: VaultPassword | null = null
 let vaultState: VaultState = VaultState.LOCKED
 
+/**
+ * Checks if a vault file exists
+ */
 export const vaultExists = (): boolean => fs.existsSync(VAULT_FILE_PATH)
 
+/**
+ * Creates a new vault.
+ * @param vaultPlainPassword
+ */
 export const createVault = (vaultPlainPassword: string): Vault => {
   const salt = crypto.createHash('sha256').update(crypto.randomBytes(16)).digest()
   const password = passwordHash(vaultPlainPassword, salt)
@@ -40,6 +47,12 @@ export const createVault = (vaultPlainPassword: string): Vault => {
   return vault
 }
 
+/**
+ * Generates a new password-based derived key from a given password/salt combination.
+ * If no salt is provided, than a new random one is used.
+ * @param vaultPassword
+ * @param currentSalt
+ */
 export const passwordHash = (
   vaultPassword: string,
   currentSalt: Buffer | null = null,
@@ -51,6 +64,10 @@ export const passwordHash = (
   }
 }
 
+/**
+ * Unlocks the vault, bringing the contents from disk to the memory (unencrypted).
+ * @param vaultPlainPassword
+ */
 export const unlockVault = (vaultPlainPassword: string): boolean => {
   try {
     const vaultWrapper: VaultWrapper = JSON.parse(
@@ -74,12 +91,20 @@ export const unlockVault = (vaultPlainPassword: string): boolean => {
   }
 }
 
+/**
+ * Completely removes the in-memory vault, session password-based derived key and changes
+ * the state to locked.
+ */
 export const lockVault = (): void => {
   vault = null
   vaultPassword = null
   vaultState = VaultState.LOCKED
 }
 
+/**
+ * Persists the contents of the in-memory vault to the disk. The contents on disk
+ * will be stored encrypted using the current session password-based derived key.
+ */
 export const updateVault = (): void => {
   if (vaultState === VaultState.LOCKED || vaultPassword === null)
     throw new Error('The vault is locked!')
@@ -100,6 +125,10 @@ export const updateVault = (): void => {
   fs.writeFileSync(VAULT_FILE_PATH, JSON.stringify(vaultWrapper), { encoding: 'utf-8' })
 }
 
+/**
+ * Generates a random strong password
+ * @param options
+ */
 export const generateStrongPassword = (options: GeneratePasswordOptions) =>
   passwordGenerator.generate({
     length: options.size,
@@ -111,6 +140,10 @@ export const generateStrongPassword = (options: GeneratePasswordOptions) =>
     exclude: options.exclude,
   })
 
+/**
+ * Checks the strength of a given password
+ * @param password
+ */
 export const validateStrongPassword = (password: string) => {
   if (password.length < 8) return false
   if (password.match(/^[^0-9]+$/)) return false
@@ -122,16 +155,26 @@ export const validateStrongPassword = (password: string) => {
 
 export const getVaultState = (): VaultState => vaultState
 
+/**
+ * Returns a frozen list of credentials. This list cannot be changed.
+ */
 export const getCredentials = (): ReadonlyArray<Credential> => {
   if (!vault || vaultState === VaultState.LOCKED) throw new Error('Vault is locked')
   return Object.freeze([...vault.credentials])
 }
 
+/**
+ * Returns the size (number of credentials) of the vault
+ */
 export const getSize = (): number => {
   if (!vault || vaultState === VaultState.LOCKED) throw new Error('Vault is locked')
   return vault.totalItems
 }
 
+/**
+ * Adds a new credential and updates the vault on disk
+ * @param credential
+ */
 export const addCredential = (credential: Credential): void => {
   if (vaultState === VaultState.LOCKED || vault === null) throw new Error('Vault is locked')
   if (vault.credentials.some((c) => c.identifier === credential.identifier))
@@ -142,21 +185,37 @@ export const addCredential = (credential: Credential): void => {
   updateVault()
 }
 
+/**
+ * Deletes a credential and updates the vault on disk
+ * @param credential
+ */
 export const removeCredential = (credential: Credential): void => {
   if (vaultState === VaultState.LOCKED || vault === null) throw new Error('Vault is locked')
   vault.credentials = vault.credentials.filter((c) => c !== credential)
   updateVault()
 }
 
+/**
+ * Checks if the provided plain password is the valid one for the vault
+ * @param plainPassword
+ */
 export const checkCurrentPassword = (plainPassword: string): boolean => {
   if (vaultPassword === null) throw new Error('Unexpected null vault password')
   const hash = passwordHash(plainPassword, vaultPassword.salt)
   return hash.hash.equals(vaultPassword.hash as Buffer)
 }
 
+/**
+ * Changes the session hashed password and updates the vault so the encrypted content
+ * will now be using the new password as the source for the key
+ * @param plainPassword
+ */
 export const changePassword = (plainPassword: string): void => {
   vaultPassword = passwordHash(plainPassword)
   updateVault()
 }
 
+/**
+ * Checks if the vault is currently in a lock state
+ */
 export const locked = (): boolean => vaultState === VaultState.LOCKED
